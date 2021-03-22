ffb6d7e819d6c3a6d7a5916ca97da286f7c7a306
unknown <rondinonstefi@gmail.com>
2021-03-22 03:07:25 -0300

    TRIVIA-QUIZ-01

---
+++ main.js
@@ -0,0 +1,252 @@
+
+
+
+
+function desplegarOpcion(numero){
+    if (numero == 1){
+        document.getElementById('pregunta1').style.opacity = "1";
+        document.getElementById('button1').style.background = "#1f0841"
+        document.getElementById('button1').style.color = "#fff"
+    }
+    else if (numero == 2){
+        document.getElementById('pregunta2').style.opacity = "1";
+        document.getElementById('button2').style.background = "#1f0841"
+        document.getElementById('button2').style.color = "#fff"
+    }
+    else if (numero == 3){
+        document.getElementById('pregunta3').style.opacity = "1";
+        document.getElementById('button3').style.background = "#1f0841"
+        document.getElementById('button3').style.color = "#fff"
+    }
+    else if (numero == 4){
+        document.getElementById('pregunta4').style.opacity = "1";
+        document.getElementById('button4').style.background = "#1f0841"
+        document.getElementById('button4').style.color = "#fff"
+    }
+    else if (numero == 5){
+        document.getElementById('pregunta5').style.opacity = "1";
+        document.getElementById('button5').style.background = "#1f0841"
+        document.getElementById('button5').style.color = "#fff"
+    }
+    else if (numero == 6){
+        document.getElementById('pregunta6').style.opacity = "1";
+        document.getElementById('button6').style.background = "#1f0841"
+        document.getElementById('button6').style.color = "#fff"
+    }
+    else if (numero == 7){
+        document.getElementById('pregunta7').style.opacity = "1";
+        document.getElementById('button7').style.background = "#1f0841"
+        document.getElementById('button7').style.color = "#fff"
+    }
+    else if (numero == 8){
+        document.getElementById('pregunta8').style.opacity = "1";
+        document.getElementById('button8').style.background = "#1f0841"
+        document.getElementById('button8').style.color = "#fff"
+    }
+    else if (numero == 9){
+        document.getElementById('pregunta9').style.opacity = "1";
+        document.getElementById('button9').style.background = "#1f0841"
+        document.getElementById('button9').style.color = "#fff"
+    }
+    else if (numero == 10){
+        document.getElementById('pregunta10').style.opacity = "1";
+        document.getElementById('button10').style.background = "#1f0841"
+        document.getElementById('button10').style.color = "#fff"
+    }
+    else if (numero == 11){
+        document.getElementById('pregunta11').style.opacity = "1";
+        document.getElementById('button11').style.background = "#1f0841"
+        document.getElementById('button11').style.color = "#fff"
+    }
+    else if (numero == 12){
+        document.getElementById('pregunta12').style.opacity = "1";
+        document.getElementById('button12').style.background = "#1f0841"
+        document.getElementById('button12').style.color = "#fff"
+    }
+    else if (numero == 13){
+        document.getElementById('pregunta13').style.opacity = "1";
+        document.getElementById('button13').style.background = "#1f0841"
+        document.getElementById('button13').style.color = "#fff"
+    }
+    else if (numero == 14){
+        document.getElementById('pregunta14').style.opacity = "1";
+        document.getElementById('button14').style.background = "#1f0841"
+        document.getElementById('button14').style.color = "#fff"
+    }
+    else if (numero == 15){
+        document.getElementById('pregunta15').style.opacity = "1";
+        document.getElementById('button15').style.background = "#1f0841"
+        document.getElementById('button15').style.color = "#fff"
+    }
+    else if (numero == 16){
+        document.getElementById('pregunta16').style.opacity = "1";
+        document.getElementById('button16').style.background = "#1f0841"
+        document.getElementById('button16').style.color = "#fff"
+    }
+    else if (numero == 17){
+        document.getElementById('pregunta17').style.opacity = "1";
+        document.getElementById('button17').style.background = "#1f0841"
+        document.getElementById('button17').style.color = "#fff"
+    }
+    else if (numero == 18){
+        document.getElementById('pregunta18').style.opacity = "1";
+        document.getElementById('button18').style.background = "#1f0841"
+        document.getElementById('button18').style.color = "#fff"
+    }
+    else if (numero == 19){
+        document.getElementById('pregunta19').style.opacity = "1";
+        document.getElementById('button19').style.background = "#1f0841"
+        document.getElementById('button19').style.color = "#fff"
+    }
+    else if (numero == 20){
+        document.getElementById('pregunta20').style.opacity = "1";
+        document.getElementById('button20').style.background = "#1f0841"
+        document.getElementById('button20').style.color = "#fff"
+    }
+    else if (numero == 21){
+        document.getElementById('pregunta21').style.opacity = "1";
+        document.getElementById('button21').style.background = "#1f0841"
+        document.getElementById('button21').style.color = "#fff"
+    }
+    else if (numero == 22){
+        document.getElementById('pregunta22').style.opacity = "1";
+        document.getElementById('button22').style.background = "#1f0841"
+        document.getElementById('button22').style.color = "#fff"
+    }
+    else if (numero == 23){
+        document.getElementById('pregunta23').style.opacity = "1";
+        document.getElementById('button23').style.background = "#1f0841"
+        document.getElementById('button23').style.color = "#fff"
+    }
+    else if (numero == 24){
+        document.getElementById('pregunta24').style.opacity = "1";
+        document.getElementById('button24').style.background = "#1f0841"
+        document.getElementById('button24').style.color = "#fff"
+    }
+    else if (numero == 25){
+        document.getElementById('pregunta25').style.opacity = "1";
+        document.getElementById('button25').style.background = "#1f0841"
+        document.getElementById('button25').style.color = "#fff"
+    }
+    else if (numero == 26){
+        document.getElementById('pregunta26').style.opacity = "1";
+        document.getElementById('button26').style.background = "#1f0841"
+        document.getElementById('button26').style.color = "#fff"
+    }
+    else if (numero == 27){
+        document.getElementById('pregunta27').style.opacity = "1";
+        document.getElementById('button27').style.background = "#1f0841"
+        document.getElementById('button27').style.color = "#fff"
+    }
+    else if (numero == 28){
+        document.getElementById('pregunta28').style.opacity = "1";
+        document.getElementById('button28').style.background = "#1f0841"
+        document.getElementById('button28').style.color = "#fff"
+    }
+    else if (numero == 29){
+        document.getElementById('pregunta29').style.opacity = "1";
+        document.getElementById('button29').style.background = "#1f0841"
+        document.getElementById('button29').style.color = "#fff"
+    }
+    else if (numero == 30){
+        document.getElementById('pregunta30').style.opacity = "1";
+        document.getElementById('button30').style.background = "#1f0841"
+        document.getElementById('button30').style.color = "#fff"
+    }
+    else if (numero == 31){
+        document.getElementById('pregunta31').style.opacity = "1";
+        document.getElementById('button31').style.background = "#1f0841"
+        document.getElementById('button31').style.color = "#fff"
+    }
+    else if (numero == 32){
+        document.getElementById('pregunta32').style.opacity = "1";
+        document.getElementById('button32').style.background = "#1f0841"
+        document.getElementById('button32').style.color = "#fff"
+    }
+    else if (numero == 33){
+        document.getElementById('pregunta33').style.opacity = "1";
+        document.getElementById('button33').style.background = "#1f0841"
+        document.getElementById('button33').style.color = "#fff"
+    }
+    else if (numero == 34){
+        document.getElementById('pregunta34').style.opacity = "1";
+        document.getElementById('button34').style.background = "#1f0841"
+        document.getElementById('button34').style.color = "#fff"
+    }
+    else if (numero == 35){
+        document.getElementById('pregunta35').style.opacity = "1";
+        document.getElementById('button35').style.background = "#1f0841"
+        document.getElementById('button35').style.color = "#fff"
+    }
+    else if (numero == 36){
+        document.getElementById('pregunta36').style.opacity = "1";
+        document.getElementById('button36').style.background = "#1f0841"
+        document.getElementById('button36').style.color = "#fff"
+    }
+}
+
+
+function numeroAleatorio(){
+    let numero = generarNumero(1, 37)
+    let numeroParseado = parseInt(numero)
+    document.getElementById("parrafo_num").innerHTML = numeroParseado;
+}
+
+function generarNumero(min, max){
+    // Retorna un número aleatorio entre min (incluido) y max (excluido)
+    return Math.random() * (max - min) + min;
+}
+
+
+function nombreAleatorio(){
+    let persona;
+    let num = generarNumero(1, 14)
+    let numeroParseado = parseInt(num)
+    if (numeroParseado == 1){
+        persona = "Stefi"
+    }else if(numeroParseado == 2){
+        persona = "Dani"
+    }
+    else if(numeroParseado == 3){
+        persona = "Jesi"
+    }
+    else if(numeroParseado == 4){
+        persona = "Lore"
+    }
+    else if(numeroParseado == 5){
+        persona = "Mara"
+    }
+    else if(numeroParseado == 6){
+        persona = "Santi"
+    }
+    else if(numeroParseado == 7){
+        persona = "Debo"
+    }
+    else if(numeroParseado == 8){
+        persona = "Sofi"
+    }
+    else if(numeroParseado == 9){
+        persona = "May"
+    }
+    else if(numeroParseado == 10){
+        persona = "Steph"
+    }
+    else if(numeroParseado == 11){
+        persona = "Lau"
+    }
+    else if(numeroParseado == 12){
+        persona = "Pato"
+    }
+    else if(numeroParseado == 13){
+        persona = "Joni"
+    }
+    else{
+        console.log("esto no se tendria que leer")
+    }
+    document.getElementById("parrafo_nom").innerHTML = persona;
+}
+
+function generarNombre(min, max){
+    // Retorna un número aleatorio entre min (incluido) y max (excluido)
+    return Math.random() * (max - min) + min;
+}
